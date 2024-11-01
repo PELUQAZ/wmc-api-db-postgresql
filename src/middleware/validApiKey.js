7274e355132b0d8734165da0dbc3a5f483cf01ab
Lukas Vallejo Meluk <emlukas82@hotmail.com>
2024-11-01 04:39:47 +0100

    Ajuste API KEY

--- src/middleware/validApiKey.js
+++ src/middleware/validApiKey.js
@@ -1,13 +1,17 @@
-//require('dotenv').config();
-
 const validateApiKey = (req, res, next) => {
     const apiKey = req.headers['x-api-key'];
     const validApiKey = process.env.API_KEY;
 
+    // Verifica si la API_KEY está definida
+    if (!validApiKey) {
+        console.error("API Key no está configurada en las variables de entorno.");
+        return res.status(500).json({ message: "Server configuration error: API Key not set" });
+    }
+
     if (apiKey && apiKey === validApiKey) {
-        next(); // API Key válida
+        next(); // Continúa si la API Key es válida
     } else {
-        res.status(401).json({ message: 'Unauthorized' });
+        res.status(401).json({ message: 'Unauthorized' }); // Devuelve error si no es válida
     }
 };
 
