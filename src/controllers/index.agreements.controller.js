16984003431e5d4a281fe5d726f0690a44076b08
Lukas Vallejo Meluk <emlukas82@hotmail.com>
2025-01-24 16:06:13 +0100

    Prueba getAgreements con select *

--- src/controllers/index.agreements.controller.js
+++ src/controllers/index.agreements.controller.js
@@ -2,15 +2,15 @@ import { pool } from "../db.js";
 
 //Obtener acuerdos
 export const getAgreements = async (req, res) => {
-  //const response = await pool.query("SELECT * FROM tx_acuerdos ORDER BY id ASC");
-  const response = await pool.query("SELECT a.id, a.servicio_id, a.horas, a.monto, a.fecha_inicio, a.estado_arbitraje, a.fecha_fin, a.address_sc," + 
+  const response = await pool.query("SELECT * FROM tx_acuerdos ORDER BY id ASC");
+  /*const response = await pool.query("SELECT a.id, a.servicio_id, a.horas, a.monto, a.fecha_inicio, a.estado_arbitraje, a.fecha_fin, a.address_sc," + 
     "a.fecha_acuerdo, a.tipo_token, a.acuerdo_id_sc, a.pagador_de_acuerdo, a.proveedor_de_acuerdo, a.hash_tx, a.id_pagador," +
     "upa.wallet AS wallet_pagador, a.id_proveedor, upr.wallet AS wallet_proveedor, a.id_arbitro, ua.wallet AS wallet_arbitro " +
     "FROM tx_acuerdos a " +
     "LEFT JOIN usuarios_wmc upa ON a.id_pagador = upa.id " +
     "LEFT JOIN usuarios_wmc upr ON a.id_proveedor = upr.id" +
     "LEFT JOIN usuarios_wmc ua ON a.id_arbitro = ua.id"
-  );
+  );*/
   res.status(200).json(response.rows);
 };
 
